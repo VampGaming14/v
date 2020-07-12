691fbbdb92a9d163502d0fcaa97297642a068a58
VampGaming14 <65399588+VampGaming14@users.noreply.github.com>
2020-07-12 20:25:26 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,12 @@
+class paper{
+    constructor(x, y, width, height, angle) {
+		var options={
+		     isStatic=false, 
+            restitution:0.3,
+           friction:0.5,
+		   density:1.2
+		}
+    }
+    
+		 
+    }
---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+function preload()
+{
+	var options = {
+		isStatic=false,
+		restitution:0.3,
+		friction:0.5,
+		density:1.2
+		 
+    }
+}
+
+function setup() {
+	createCanvas(800, 700);
+   
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	World.add(world, packageBody);
+	elipse(56,46,55,55);
+	packageIMG = loadImage("download/paper");
+	
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
